91b7d6bed9e35049f9191b4bcc1066da8664f681
Bruno Magalhães <106349470+brunozer0@users.noreply.github.com>
2022-06-18 23:13:42 -0300

    att

---
+++ estruturas de repetição/ambiente.js
@@ -0,0 +1,36 @@
+
+/*
+var c = 1 
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 2)
+
+var c = 1
+while (c <= 3) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
+
+/*
+var c = 1
+while (c<= 5) {
+    console.log(`olá`)
+  c++
+}
+*/
+console.log(`vai começar`)
+for(var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('fim')
